ed3ed5d36471208b3752a88145cd69eae4a7f0fd
Housseine-che <Housseinecherif100@gmail.com>
2025-05-22 12:29:36 +0000

    Create text.py

---
+++ text.py
@@ -0,0 +1,27 @@
+# a= int(3.14)
+# print(a)
+# print('**************************')
+# b=str(10)
+# print(b)
+# print(type(a))
+# nom='Diallo'
+# age= 25
+# print(isinstance(nom,int))
+# print("Hello","all",sep='-')
+# print("bonjour",end="!")
+# print('nom:{},age:{}'.format(nom,age))
+# nom = input('Quel est votre nom svp:')
+# age = input('Donnez également ton age:')
+# taille = input('Essayez de nous fournir ta taille:')
+# taille =float(taille)
+# print(f" Bonjour mon nom est: {nom} et j'ai {age} ans !")
+# print(f" Ma taille mesure de {taille} m")
+# Gestion des erreurs:
+# try:
+#    age = int(input('Donnez votre age:'))
+#    print(f"Alors votre age est: {age} ans !")
+# except ValueError:
+#    print("Erreur: Veuillez entrer des carrectaires correctes !")
+a = 5
+b = 7
+print('La puissance de a à b est égale : ',a ** b)
